66154374d313de86cabeddd1ef03b036ee13dc7e
Pedro Lucas Soares da Silva <pedrolucassoaresds@gmail.com>
2024-05-08 03:04:21 -0400

    style #2: trivial adjust on script's function style
    Just removing some extra indentation which was not needed.
    
    Signed-off-by: Pedro Lucas Soares da Silva <pedrolucassoaresds@gmail.com>

--- src/js/app.js
+++ src/js/app.js
@@ -815,9 +815,9 @@ function Inicio(){
 document.getElementById('CPF').addEventListener('input', function(e) {
   var value = e.target.value;
   var cpfPattern = value.replace(/\D/g, '')
-						.replace(/(\d{3})(\d)/, '$1.$2')
-						.replace(/(\d{3})(\d)/, '$1.$2')
-						.replace(/(\d{3})(\d)/, '$1-$2')
-						.replace(/(-\d{2})\d+?$/, '$1');
+	  .replace(/(\d{3})(\d)/, '$1.$2')
+	  .replace(/(\d{3})(\d)/, '$1.$2')
+	  .replace(/(\d{3})(\d)/, '$1-$2')
+	  .replace(/(-\d{2})\d+?$/, '$1');
   e.target.value = cpfPattern;
 });
